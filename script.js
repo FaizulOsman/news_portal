70674dbcc931e05455656d02534b9da15bebf2b3
FaizulOsman <faizulosman@gmail.com>
2022-09-03 03:44:42 +0100

    details add in modal

--- script.js
+++ script.js
@@ -64,7 +64,9 @@ const category = async (id) => {
                             <i class="fa-regular fa-star"></i>
                         </div>
                         <div class=" my-2">
-                            <label for="my-modal-3" class="btn modal-button"><i onclick="newsModal()" class="fa-solid fa-arrow-right"></i></label>
+                            <label onclick="newsModal('${title}','${name === null || name === '' ? 'No data available' : name}', '${img}', '${total_view === null ? 'No data available' : total_view}','${published_date === null ? 'No Published Date' : published_date}')" for="my-modal-3" class="btn modal-button">
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </label>
                         </div>
                     </div>
                 </div>
@@ -74,29 +76,21 @@ const category = async (id) => {
     }
 }
 
-const newsModal = () => {
+const newsModal = (title, name, img, total_view, published_date) => {
     const modal = document.getElementById('modal-area')
     modal.innerHTML = `
+    <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+    <div class="modal">
         <div class="modal-box relative">
             <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-            <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for
-                free!</p>
+            <h3 class="text-lg font-bold mr-5">${title}</h3>
+            <img class="py-4" src="${img}">
+            <div class="py-4 flex justify-between">
+                <p>${name}</p>
+                <p>${total_view}</p>
+                <p>${published_date}</p>                               
+            </div>
         </div>
+    </div>
     `
-
-
-
-    //     <!-- The button to open modal -->
-    // <label for="my-modal-3" class="btn modal-button">open modal</label>
-
-    // <!-- Put this part before </body> tag -->
-    // <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-    // <div class="modal">
-    //   <div class="modal-box relative">
-    //     <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-    //     <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-    //     <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    //   </div>
-    // </div>
 }
